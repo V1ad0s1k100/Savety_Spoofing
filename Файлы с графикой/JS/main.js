592854cee3c02c65a08d86d0028b7da3132d51f0
V1ad0s1k <lyubimovladislav@yandex.ru>
2025-01-31 19:15:56 +0700

    Увеличен отступ в CSS и добавлены комментарии в JavaScript для улучшения читаемости кода

--- Файлы с графикой/JS/main.js
+++ Файлы с графикой/JS/main.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { exec } = require("child_process");
+const { app, BrowserWindow, ipcMain } = require("electron"); // Импортируем необходимые модули из Electron
+const { exec } = require("child_process"); // Импортируем модуль для выполнения команд в командной строке
 
-let isChecking = false;
-let checkInterval;
+let isChecking = false; // Флаг для отслеживания состояния проверки
+let checkInterval; // Переменная для хранения интервала проверки
 
 function createWindow() {
+  // Функция для создания окна приложения
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -15,35 +16,39 @@ function createWindow() {
     autoHideMenuBar: true,
   });
 
-  win.loadFile("index.html");
+  win.loadFile("index.html"); // Загружаем HTML файл в окно
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow); // Создаем окно, когда приложение готово
 
 app.on("window-all-closed", () => {
+  // Закрываем приложение, если все окна закрыты (кроме macOS)
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
+  // Создаем новое окно, если приложение активировано и нет открытых окон (для macOS)
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
 ipcMain.on("start-check", (event) => {
+  // Обработчик события для начала/остановки проверки
   if (isChecking) {
-    clearInterval(checkInterval);
+    clearInterval(checkInterval); // Останавливаем интервал проверки
     isChecking = false;
-    event.sender.send("check-status", "Check stopped");
+    event.sender.send("check-status", "Check stopped"); // Отправляем статус в рендерер
     console.log("Check stopped");
   } else {
     isChecking = true;
-    event.sender.send("check-status", "Check started");
+    event.sender.send("check-status", "Check started"); // Отправляем статус в рендерер
     console.log("Check started");
 
     function checkDuplicates(arpTable) {
+      // Функция для проверки дубликатов MAC-адресов
       let macDict = {};
       let duplicates = [];
 
@@ -65,6 +70,7 @@ ipcMain.on("start-check", (event) => {
     }
 
     function handleDuplicates() {
+      // Функция для обработки дубликатов MAC-адресов
       const platform = process.platform;
       try {
         if (platform === "win32") {
@@ -86,6 +92,7 @@ ipcMain.on("start-check", (event) => {
     }
 
     checkInterval = setInterval(() => {
+      // Устанавливаем интервал для выполнения проверки каждые 2 секунды
       exec("arp -a", (error, stdout) => {
         if (error) {
           console.error(`exec error: ${error}`);
