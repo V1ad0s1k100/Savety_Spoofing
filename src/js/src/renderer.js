8b2b301d467af5f93c48909b3083cb4f2e8be7cc
V1ad0s1k <lyubimovladislav@yandex.ru>
2025-03-27 23:16:47 +0700

    Удаление устаревших стилей шапки, добавление минимальных размеров окна и реализация функционала для панели логов

---
+++ src/js/src/renderer.js
@@ -0,0 +1,43 @@
+const historyElement = document.querySelector("[data-logs-history]");
+const logsBar = document.querySelector("[data-logs-bar]");
+
+let historyCount = 0;
+
+historyElement.addEventListener("click", () => {
+  logsBar.classList.toggle("logs-bar-block");
+});
+
+const stickCloseConfigElement = document.querySelector("[data-stick-close]");
+const settingsBarElement = document.querySelector("[data-settings-bar]");
+const btnOpenConfigElement = document.querySelector("[data-nav-btn]");
+
+stickCloseConfigElement.addEventListener("click", () => {
+  settingsBarElement.style.display = "none";
+});
+
+btnOpenConfigElement.addEventListener("click", () => {
+  settingsBarElement.style.display = "flex";
+});
+
+const dateElement = document.querySelector("[data-working-time]");
+
+const timerFn = () => {
+  dateElement.textContent = "00:00:01";
+  let seconds = 1;
+
+  const timer = setInterval(() => {
+    seconds++;
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0");
+    const secs = String(seconds % 60).padStart(2, "0");
+    dateElement.textContent = `${hours}:${minutes}:${secs}`;
+  }, 1000);
+};
+
+const mainBtnElement = document.querySelector("[data-main-btn]");
+const loadingStatusElement = document.querySelector("[data-status-loading]");
+
+mainBtnElement.addEventListener("click", () => {
+  timerFn();
+  loadingStatusElement.textContent = "There is an analysis";
+});
