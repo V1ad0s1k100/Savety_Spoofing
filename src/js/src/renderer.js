3746cc1fb4e599d30ae379188a9becea2307d260
V1ad0s1k <lyubimovladislav@yandex.ru>
2025-03-28 13:36:45 +0700

    Удаление устаревших стилей и файлов, обновление импорта стилей для десктопной версии, добавление анимации перехода в CSS и улучшение логики таймера в JavaScript

--- src/js/src/renderer.js
+++ src/js/src/renderer.js
@@ -7,6 +7,8 @@ historyElement.addEventListener("click", () => {
   logsBar.classList.toggle("logs-bar-block");
 });
 
+// =================================================
+
 const stickCloseConfigElement = document.querySelector("[data-stick-close]");
 const settingsBarElement = document.querySelector("[data-settings-bar]");
 const btnOpenConfigElement = document.querySelector("[data-nav-btn]");
@@ -19,25 +21,37 @@ btnOpenConfigElement.addEventListener("click", () => {
   settingsBarElement.style.display = "flex";
 });
 
+// =================================
+
 const dateElement = document.querySelector("[data-working-time]");
-
-const timerFn = () => {
-  dateElement.textContent = "00:00:01";
-  let seconds = 1;
-
-  const timer = setInterval(() => {
-    seconds++;
-    const hours = String(Math.floor(seconds / 3600)).padStart(2, "0");
-    const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0");
-    const secs = String(seconds % 60).padStart(2, "0");
-    dateElement.textContent = `${hours}:${minutes}:${secs}`;
-  }, 1000);
-};
 
 const mainBtnElement = document.querySelector("[data-main-btn]");
 const loadingStatusElement = document.querySelector("[data-status-loading]");
 
+let timeCount = 0;
+let timer = null;
+
 mainBtnElement.addEventListener("click", () => {
-  timerFn();
-  loadingStatusElement.textContent = "There is an analysis";
+  if (timer === null) {
+    dateElement.textContent = "00:00:01";
+    let seconds = 1;
+
+    timer = setInterval(() => {
+      seconds++;
+      const hours = String(Math.floor(seconds / 3600)).padStart(2, "0");
+      const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(
+        2,
+        "0"
+      );
+      const secs = String(seconds % 60).padStart(2, "0");
+      dateElement.textContent = `${hours}:${minutes}:${secs}`;
+    }, 1000);
+
+    loadingStatusElement.textContent = "There is an analysis";
+  } else {
+    clearInterval(timer);
+    timer = null;
+    dateElement.textContent = "00:00:00";
+    loadingStatusElement.textContent = "Analysis stopped";
+  }
 });
